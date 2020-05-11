e4637c4cb066fc5a844e13cbab3b83d97cbc8556
yuri-ming <lul6959@naver.com>
2020-05-11 18:18:10 +0900

    board

--- project1/board/urls.py
+++ project1/board/urls.py
@@ -6,6 +6,13 @@ from . import views
 
 urlpatterns =[
 	path('',views.home, name="home"),
+	path('board',views.board, name="board"),
+	path('board_write',views.board_write, name="board_write"),
+	path('board_insert',views.board_insert, name="board_insert"),
+	path('board_view',views.board_view, name="board_view"),
+	path('board_edit',views.board_edit, name="board_edit"),
+	path('board_update',views.board_update, name="board_update"),
+	path('board_delete',views.board_delete, name="board_delete"),
 	]
 
 
--- project1/board/views.py
+++ project1/board/views.py
@@ -1,5 +1,78 @@
-from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import render, redirect
+from pip._vendor.requests import Response
+
+from board.models import Board
 
 # Create your views here.
 def home(request):
     return render(request,"home.html")
+
+def board(request):
+    rsBoard=Board.objects.all()
+
+    return render(request, "board_list.html",{
+        'rsBoard': rsBoard
+    })
+def board_write(request):
+    return render(request,"board_write.html", )
+
+def board_insert(request):
+    btitle=request.GET['b_title']
+    bnote = request.GET['b_note']
+    bwriter = request.GET['b_writer']
+
+    if btitle != "":
+        rows=Board.objects.create(b_title=btitle, b_note=bnote, b_writer=bwriter)
+        return redirect('/board')
+    else:
+        return redirect('/board_write')
+
+def board_view(request):
+    bno=request.GET['b_no']
+    rsDetail=Board.objects.filter(b_no=bno)
+
+    return render(request,"board_view.html",{
+        'rsDetail':rsDetail
+    })
+
+def board_edit(request):
+    bno=request.GET['b_no']
+    rsDetail = Board.objects.filter(b_no=bno)
+
+    return render(request,"board_edit.html",{
+        'rsDetail':rsDetail
+    })
+
+
+
+
+def board_update(request):
+    bno= request.GET['b_no']
+    btitle = request.GET['b_title']
+    bnote = request.GET['b_note']
+    bwriter = request.GET['b_writer']
+    
+    try:
+        board = Board.objects.get(b_no=bno)
+        if btitle !="":
+            board.b_title = btitle
+        if bnote != "":
+            board.b_note = bnote
+        if bwriter !="":
+            board.b_writer = bwriter
+            
+        try:
+            board.save()
+            return redirect('/board')
+        except ValueError:
+            return Response({"success":False, "msg":"에러입니다."})
+        
+    except ObjectDoesNotExist:
+        return Response({"success":False, "msg": "게시글 없음"})
+
+def board_delete(request):
+    bno = request.GET['b_no']
+    rows=Board.objects.get(b_no=bno).delete()
+
+    return redirect('/board')
